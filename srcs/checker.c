c95cbc6394818dc2a97beff6bb0eb80733fdb7b3
hugothms <hugothms@gmail.com>
2021-03-09 11:20:02 +0100

    rotate + reverse + comments

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,14 +6,15 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 15:04:19 by hthomas           #+#    #+#             */
-/*   Updated: 2021/03/09 10:57:47 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/03/09 11:19:32 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/checker.h"
 
 /**
- * if the given array is sorted return 0, else return 1
+ * Check if the given list is sorted
+ * @return	0 if the list is sorted, otherwise return 1
  **/
 
 int		checker(t_dlist *list)
@@ -33,6 +34,11 @@ int		checker(t_dlist *list)
 	return (0);
 }
 
+/**
+ * Swap the ﬁrst 2 elements at the top of stack
+ * Do nothing if there is only one or no elements)
+ **/
+
 void	swap(t_dlist **list)
 {
 	int		*tmp;
@@ -45,22 +51,41 @@ void	swap(t_dlist **list)
 	}
 }
 
+/**
+ * Take the ﬁrst element at the top of b and put it at the top of a 
+ * Do nothing if b is empty
+ **/
+
 void	push(t_dlist **a, t_dlist **b)
 {
 	t_dlist	*tmp;
-	if (*b)
-	{
-		tmp = ft_dlstremove_one(*b);
+	
+	if ((tmp = ft_dlstremove_one(*b)))
 		ft_dlstadd_front(a, tmp);
-	}
 }
 
-// void	rotate(t_dlist **list)
-// {
+/**
+ * Shift up all elements of stack a by 1 
+ * The ﬁrst element becomes the last one
+ **/
 
-// }
+void	rotate(t_dlist **list)
+{
+	t_dlist	*tmp;
 
-// void	reverse(t_dlist **list)
-// {
+	if ((tmp = ft_dlstremove_one(*list)))
+		ft_dlstadd_back(list, tmp);
+}
 
-// }
+/**
+ * shift down all elements of stack a by 1
+ * The last element becomes the ﬁrst one.
+ **/
+
+void	reverse(t_dlist **list)
+{
+	t_dlist	*tmp;
+
+	if ((tmp = ft_dlstremove_one(ft_dlstlast(*list))))
+		ft_dlstadd_front(list, tmp);
+}

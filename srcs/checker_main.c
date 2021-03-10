70fc04d6f17cac182722342f34d20b41ad7fd76a
Hugothms <hugothms@gmail.com>
2021-03-10 17:05:56 +0100

    ca decoupe les fonctions

--- srcs/checker_main.c
+++ srcs/checker_main.c
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 15:04:19 by hthomas           #+#    #+#             */
-/*   Updated: 2021/03/10 16:36:56 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/03/10 17:04:32 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,64 +29,90 @@ int		scan_args(int argc, char const *argv[], t_dlist **a)
 	return (0);
 }
 
+void	swap_it(t_dlist **a, t_dlist **b, char *line, int *ret)
+{
+	*ret = 0;
+	if (!ft_strcmp(line, "sa"))
+		swap(a);
+	else if (!ft_strcmp(line, "sb"))
+		swap(b);
+	else if (!ft_strcmp(line, "ss"))
+	{
+		swap(a);
+		swap(b);
+	}
+	else
+		*ret = 1;
+}
+
+void	push_it(t_dlist **a, t_dlist **b, char *line, int *ret)
+{
+	*ret = 0;
+	if (!ft_strcmp(line, "pa"))
+		push(a, b);
+	else if (!ft_strcmp(line, "pb"))
+		push(b, a);
+	else
+		*ret = 1;
+}
+
+void	rotate_it(t_dlist **a, t_dlist **b, char *line, int *ret)
+{
+	*ret = 0;
+	if (!ft_strcmp(line, "ra"))
+		rotate(a);
+	else if (!ft_strcmp(line, "rb"))
+		rotate(b);
+	else if (!ft_strcmp(line, "rr"))
+	{
+		rotate(a);
+		rotate(b);
+	}
+	else
+		*ret = 1;
+}
+void	reverse_it(t_dlist **a, t_dlist **b, char *line, int *ret)
+{
+	*ret = 0;
+	if (!ft_strcmp(line, "rra"))
+		reverse(a);
+	else if (!ft_strcmp(line, "rrb"))
+		reverse(b);
+	else if (!ft_strcmp(line, "rrr"))
+	{
+		reverse(a);
+		reverse(b);
+	}
+	else
+		*ret = 1;
+}
+
+
 int		main(int argc, char const *argv[])
 {
 	t_dlist	*a;
 	t_dlist	*b;
+	char	*line;
+	int		ret;
 
 	b = NULL;
 	if (scan_args(argc, argv, &a))
 		return (1);
-
-	// t_dlist	*tmp1;
-	// t_dlist	*tmp;
-	// int		*num;
-	// if (!(num = malloc(sizeof(int))))
-	// 	exit(1);
-	// *num = 42;
-	// ft_dlstadd_front(&a, (tmp = ft_dlstnew(num)));
-	// ft_dlstadd_front(&b, (tmp1 = ft_dlstnew(num)));
-	// ft_dlstadd_front(&b, (tmp = ft_dlstnew(num)));
-	// ft_dlstremove_one(&b, tmp1);
-	// ft_dlstremove_one(&b, tmp);
-
 	print_clean_dlist(a, b);
-	char	*line;
 	while(get_next_line(&line ,0))
 	{
 		printf("---------\nExec %s:\n", line);
-		if (!ft_strcmp(line, "sa"))
-			swap(&a);
-		else if (!ft_strcmp(line, "sb"))
-			swap(&b);
-		else if (!ft_strcmp(line, "ss"))
-		{
-			swap(&a);
-			swap(&b);
-		}
-		else if (!ft_strcmp(line, "pa"))
-			push(&a, &b);
-		else if (!ft_strcmp(line, "pb"))
-			push(&b, &a);
-		else if (!ft_strcmp(line, "ra"))
-			rotate(&a);
-		else if (!ft_strcmp(line, "rb"))
-			rotate(&b);
-		else if (!ft_strcmp(line, "rr"))
-		{
-			rotate(&a);
-			rotate(&b);
-		}
-		else if (!ft_strcmp(line, "rra"))
-			reverse(&a);
-		else if (!ft_strcmp(line, "rrb"))
-			reverse(&b);
-		else if (!ft_strcmp(line, "rrr"))
-		{
-			reverse(&a);
-			reverse(&b);
-		}
+		if (!ft_strncmp(line, "s", 1))
+			swap_it(&a, &b, line, &ret);
+		else if (!ft_strncmp(line, "p", 1))
+			push_it(&a, &b, line, &ret);
+		else if (!ft_strncmp(line, "rr", 2))
+			reverse_it(&a, &b, line, &ret);
+		else if (!ft_strncmp(line, "r", 1))
+			rotate_it(&a, &b, line, &ret);
 		else
+			exit (1);
+		if (ret)
 			exit (1);
 		print_clean_dlist(a, b);
 	}

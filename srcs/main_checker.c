59499c911c0d2a04d95f0218d292433009f172e9
hugothms <hugothms@gmail.com>
2021-03-21 02:55:52 +0100

    norm-v3

--- srcs/main_checker.c
+++ srcs/main_checker.c
@@ -6,13 +6,13 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 15:04:19 by hthomas           #+#    #+#             */
-/*   Updated: 2021/03/15 10:45:18 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/03/21 00:37:40 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int		main(int argc, char const *argv[])
+int	main(int argc, char const *argv[])
 {
 	t_dlist	*a;
 	t_dlist	*b;
@@ -22,14 +22,12 @@ int		main(int argc, char const *argv[])
 	b = NULL;
 	if (argc == 1)
 		return (0);
-	if (!(a = scan_input(argc, argv)))
+	a = scan_input(argc, argv);
+	if (!a)
 		error();
-	// print_clean_dlist(a, b);
-	// print_dlist_line(a, 'a');
-	// print_dlist_line(b, 'b');
 	while(get_next_line(&line ,0))
 	{
-		// ft_printf("\nExec %s:\n", line);
+		ft_printf("\nExec %s:\n", line);
 		if (!ft_strncmp(line, "s", 1))
 			swap_help(&a, &b, line, &ret);
 		else if (!ft_strncmp(line, "p", 1))
